b12d107ba979612ce713b6f2face7bf94b5055c2
Prayagraj Behera <86463525+prayagraj2000@users.noreply.github.com>
2022-10-21 15:39:41 +0530

    Image Preview and Login Site

---
+++ app.js
@@ -0,0 +1,85 @@
+let files = [],
+dragArea = document.querySelector('.drag-area'),
+input = document.querySelector('.drag-area input'),
+button = document.querySelector('.card button'),
+select = document.querySelector('.drag-area .select'),
+container = document.querySelector('.container');
+
+/** CLICK LISTENER */
+select.addEventListener('click', () => input.click());
+
+/* INPUT CHANGE EVENT */
+input.addEventListener('change', () => {
+	let file = input.files;
+        
+    // if user select no image
+    if (file.length == 0) return;
+         
+	for(let i = 0; i < file.length; i++) {
+        if (file[i].type.split("/")[0] != 'image') continue;
+        if (!files.some(e => e.name == file[i].name)) files.push(file[i])
+    }
+
+	showImages();
+});
+
+/** SHOW IMAGES */
+function showImages() {
+	container.innerHTML = files.reduce((prev, curr, index) => {
+		return `${prev}
+		    <div class="image">
+			    <span onclick="delImage(${index})">&times;</span>
+			    <img src="${URL.createObjectURL(curr)}" />
+			</div>`
+	}, '');
+}
+
+/* DELETE IMAGE */
+function delImage(index) {
+   files.splice(index, 1);
+   showImages();
+}
+
+/* Add */
+dragArea.addEventListener('dragover', e => {
+	e.preventDefault()
+	dragArea.classList.add('dragover')
+})
+
+/* DRAG LEAVE */
+dragArea.addEventListener('dragleave', e => {
+	e.preventDefault()
+	dragArea.classList.remove('dragover')
+});
+
+/* DROP EVENT */
+dragArea.addEventListener('drop', e => {
+	e.preventDefault()
+    dragArea.classList.remove('dragover');
+
+	let file = e.dataTransfer.files;
+	for (let i = 0; i < file.length; i++) {
+		/** Check selected file is image */
+		if (file[i].type.split("/")[0] != 'image') continue;
+		
+		if (!files.some(e => e.name == file[i].name)) files.push(file[i])
+	}
+	showImages();
+});
+const form = document.querySelector("#myform");
+
+const saveData = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    
+    saveDataToLC(Object.fromEntries(formData));
+}
+
+form.addEventListener("submit", saveData);
+
+const saveDataToLC = (data) => {
+    const records = JSON.parse(localStorage.getItem("records") ?? "[]");
+    records.push(data);
+    localStorage.setItem("records", JSON.stringify(records));
+    console.log(localStorage);
+}
